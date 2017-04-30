6e5d1d06e3bb97085f8cf6b1f54c71ebdb7324d7
Nicholas Sunjaya <nicholassunjaya@Nicholass-MacBook-Pro-6.local>
2017-04-29 17:35:10 -0700

    delete for craig's new files

--- pictures/delete/main.m
+++ pictures/delete/main.m
@@ -7,25 +7,25 @@ num_images = size(corners_dataset, 1) - 1;
 %% Check corner detector accuracy over all dataset
 good_counts = 0;
 bad_images = [];
-for i=17:34
+for i=35:49
     name = int2str(i);
     image = imread(['./pictures/' name '.jpg']);
     c_points = cornerDetector(image, false);
     d_points = corners_dataset(i+1,:);
     d_points = reshape(d_points, 2, 4);
-    good_pred = computeError(c_points, d_points, 20);
+    good_pred = computeError(c_points, d_points, 0.03*size(image,1));
     if ~good_pred
         % store all images that don't get good corners
         bad_images = [bad_images i];
         % show the image that has bad corner prediction
         displayCorners(image, c_points);
         title(['image ', name, ' with bad corners'])
-
     end
     good_counts = good_counts + good_pred;
 end
 
-accuracy = good_counts/(num_images + 1)
+accuracy = good_counts/(49-35+1)
+%accuracy = good_counts/(num_images + 1)
 
 %%
 close all
